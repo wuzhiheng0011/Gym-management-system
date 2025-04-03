bc058a2836e31f55a1960afdf8748e29b5adb6be
wuzhiheng0011 <wzh_0615@foxmail.com>
2025-04-03 15:16:03 +0800

    Delete cloudfunctions/login directory

--- cloudfunctions/login/index.js
+++
@@ -1,9 +0,0 @@
-const cloud = require('wx-server-sdk')
-cloud.init()
-
-exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  return {
-    openid: wxContext.OPENID,
-  }
-}
